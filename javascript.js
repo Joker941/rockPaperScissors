5c232c0449fdc43b323115ac7c5441c540efa4df
Joker941 <pro.julien.dussaut@gmail.com>
2023-08-04 18:41:18 +0200

    Ajout de la feuille CSS, du lien vers cette feuille dans le fichier HTML et jeu mit à l'arrêt

--- javascript.js
+++ javascript.js
@@ -19,41 +19,104 @@ function getComputerChoice() {
     }
 } 
 
-const computerChoice = getComputerChoice();
-
-const playerSelection = prompt("What do you want do play ? (Rock, paper or scissors)");
-
-//console.log(playerSelection);
+// function getUserSelection() {
+//     let playerSelection = prompt("What do you want do play ? (Rock, paper or scissors)");
+//     return playerSelection;
+// }
 
 function playRound(playerSelection, computerSelection) {
     
     playerSelection = playerSelection.toLowerCase();
-    //console.log(playerSelection);
+    
+    let resultat;
 
     if (playerSelection == "rock") {
 
         if (computerSelection == "paper") {
-            console.log("You loose ! Paper beats rock.");
+            resultat = 1;
         } else {
-            console.log("You win ! Rock beats scissors.");
+            resultat = 2;
         }
 
     } else if (playerSelection == "paper") {
 
-        if (computerSelection == "rock") {
-            console.log("You win ! Paper beats rock.");
+        if (computerSelection == "scissors") {
+            resultat = 3;
         } else {
-            console.log("You loose ! Scissors beats paper.");
+            resultat = 4;
         }
 
     } else if (playerSelection == "scissors") {
 
         if (computerSelection == "rock") {
-            console.log("You loose ! Rock beats scissors.");
+            resultat = 5;
         } else {
-            console.log("You win ! Scissors beats paper.");
+            resultat = 6;
         }
     }
+
+    return resultat;
 }
 
-playRound(playerSelection, computerChoice);
+function game() {
+
+    // Création de variable pour compter les points, une pour l'ordi, une pour l'utilisateur
+    //Boucle pour jouer 5 tours
+    //Afficher score après chaque tour
+    //Après 5 tours afficher le vainqueur
+
+    let userScore = 0;
+    let computerScore = 0;
+
+    for(i = 1; i <= 5; i++) {
+
+        let playerSelection = getUserSelection();
+        let computerSelection = getComputerChoice();
+
+        let resultatRound = playRound(playerSelection, computerSelection);
+
+        if (resultatRound == 1) {
+
+            computerScore++;
+            console.log("You loose ! Paper beats rock.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+
+        } else if (resultatRound == 2) {
+
+            userScore++;
+            console.log("You win ! Rock beats scissors.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+            
+        } else if (resultatRound == 3) {
+
+            computerScore++;
+            console.log("You loose ! Scissors beats paper.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+
+        } else if (resultatRound == 4) {
+
+            userScore++;
+            console.log("You win ! Paper beats rock.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+
+        }  else if (resultatRound == 5) {
+
+            computerScore++;
+            console.log("You loose ! Rock beats scissors.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+
+        } else if (resultatRound == 6) {
+
+            userScore++;
+            console.log("You win ! Scissors beats paper.");
+            console.log("The actual score is : computer " + computerScore + " points. You : " + userScore + " points.");
+        }
+    }
+
+    if (computerScore > userScore) {
+        console.log("The computer wins with " + computerScore + " and you have " + userScore + " points.");
+    } else {
+        console.log("You win ! You have " + userScore + " points, the computer has " + computerScore + "points.");
+    }
+}
+// game();
